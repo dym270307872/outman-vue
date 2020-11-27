8f37b032ceba24bc1922d47af08c55f8394bd6c5
独醒侠 <270307872@qq.com>
2020-11-27 17:29:17 +0800

    貌似测试了下跨域访问

--- vue-base/src/router/index.js
+++ vue-base/src/router/index.js
@@ -1,7 +1,9 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Index from '@/views/index/Index'
-import TestHttp from '@/views/demo/TestHttp.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Index from '@/views/index/Index';
+import TestHttp from '@/views/demo/TestHttp.vue';
+import TestCors from '@/views/demo/TestCors.vue';
+import TestAes from '@/views/demo/TestAes.vue';
 
 Vue.use(Router)
 
@@ -16,6 +18,16 @@ export default new Router({
       path: '/http',
       name: 'TestHttp',
       component: TestHttp
+    },
+    {
+      path: '/httpCors',
+      name: 'TestCors',
+      component: TestCors
+    },
+    {
+      path: '/httpAes',
+      name: 'TestAes',
+      component: TestAes
     }
   ]
 })
