7bb16ac09388b88b2f4df19d9d3b59db553f1349
dym270307872 <270307872@qq.com>
2021-04-14 23:35:59 +0800

    按照官网示例，导入依赖

--- vue-element/src/router/index.js
+++ vue-element/src/router/index.js
@@ -1,17 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import HelloWorld from '@/components/HelloWorld';
 
-const routes= []
- const router = new createRouter({
+const routes= [{path:'/hello',name:'hello',component: HelloWorld1}]
+const router = new createRouter({
   history:createWebHistory(),
   routes
 })
 
 router.beforeEach((to, from, next) => {
   /* 路由发生变化修改页面title */
-  if (to.meta.title) {
-    // console.log(to.component.name);
+  /* if (to.meta.title) {
+    console.log(to.component.name);
     document.title = to.meta.title
-  }
+  } */
   next()
 })
 export default router;
